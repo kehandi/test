e400d091ec3770cd480258d0282e001bbd9617f3
KeHanDi <824592535@qq.com>
2020-04-02 18:07:11 +0800

    删除没用的注释

--- src/main/java/com/bonree/Test.java
+++ src/main/java/com/bonree/Test.java
@@ -14,18 +14,7 @@ import com.bonree.util.JsonUtil;
  */
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-        /**
-         * while True:
-         *         time.sleep(20)
-         *         c_response=send_config_request()
-         *         u_response=send_upload_request()
-         *         if u_response.get("ur").get("rc")==19:
-         *             i+=1
-         *             print("request number:"+str(i))
-         *         else:
-         *             print("发送失败")
-         *             break
-         */
+
         int i = 0;
         while (true) {
             Thread.sleep(20 * 1000);
